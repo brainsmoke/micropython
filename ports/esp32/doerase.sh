55a0fda3ff6be8b7e144f1d6b983c6c64825df5e
Erik Bosman <erik@minemu.org>
2021-03-20 23:05:59 +0100

    user modules

---
+++ ports/esp32/doerase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ESPIDFDIR="../../../esp-idf"
+LEDDRIVERDIR="../../../esp32leddriver"
+
+source "$ESPIDFDIR"/export.sh
+
+make EXTRA_IDFPY_FLAGS="-DUSER_MODULES=\"$LEDDRIVERDIR/firmware/esp32/modules/cball;$LEDDRIVERDIR/firmware/esp32/modules/esphttp\"" BOARD=GENERIC_SPIRAM erase
