01a8e64e7fe46aa28a89cec53b77870555cc4265
Erik Bosman <erik@minemu.org>
2021-04-06 19:10:53 +0200

    change usermod dir

--- ports/esp32/doerase.sh
+++ ports/esp32/doerase.sh
@@ -7,4 +7,4 @@ LEDDRIVERDIR="$BUILDDIR/esp32leddriver"
 
 source "$ESPIDFDIR"/export.sh
 
-make EXTRA_IDFPY_FLAGS="-DUSER_MODULES=\"$LEDDRIVERDIR/firmware/esp32/modules/cball;$LEDDRIVERDIR/firmware/esp32/modules/esphttp\"" BOARD=GENERIC_SPIRAM erase
+make EXTRA_IDFPY_FLAGS="-DUSER_MODULES=\"$LEDDRIVERDIR/firmware/esp32/modules/cball;$LEDDRIVERDIR/firmware/esp32/modules/esphttpd\"" BOARD=GENERIC_SPIRAM erase
