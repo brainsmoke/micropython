d4f903faa6a7a3c91b0c7caf077ce9c28b599a4f
Erik Bosman <erik@minemu.org>
2021-04-18 11:04:25 +0200

    mount bug workaround (mounting twice causes errors)

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -2,12 +2,19 @@ import gc
 import uos
 from flashbdev import bdev
 
-try:
-    if bdev:
-        uos.mount(bdev, "/")
-except OSError:
-    import inisetup
+# ESP-IDF vfat mount crashes when called a second time if the first time failed
+# boot sequence adapted so that vfat mount is only called once
 
-    vfs = inisetup.setup()
+def bootsec_empty():
+    buf = bytearray(bdev.ioctl(5, 0))  # 5 is SEC_SIZE
+    bdev.readblocks(0, buf)
+    for b in buf:
+        if b != 0xFF:
+            return False
+    return True
+
+if bdev:
+    empty = bootsec_empty()
+    uos.mount(bdev, "/", mkfs=empty)
 
 gc.collect()
